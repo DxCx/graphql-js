14ac575fe3d9031fe16f3d50cb99526a5280bd6a
Hagai Cohen <hagai.co@gmail.com>
2017-04-29 19:02:24 +0300

    chore(package): add scripts to easily pull & rebase

---
+++ resources/pullNRebaseFinish.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+if [ -z ${1+x} ]; then echo "version argument not given, please state M.m.p"; exit 1; fi
+VERSION=v${1}
+BRANCH_NAME=`git symbolic-ref HEAD 2>/dev/null`
+BRANCH_NAME=${BRANCH_NAME##refs/heads/}
+if [ "${BRANCH_NAME}" != "async" ]; then echo "should be in async branch"; exit 1; fi
+rm -Rf node_modules/
+npm install
+npm test
+git push origin -f async
+git checkout -b async-${VERSION}
+git push -u origin async-${VERSION}
